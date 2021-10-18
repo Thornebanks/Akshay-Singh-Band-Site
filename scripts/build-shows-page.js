aa020578e631ff6ef7592a2d2fcd6ec419369ac5
Thornebanks <akshays.singh613@gmail.com>
2021-10-18 01:40:38 -0400

    all styling done

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -41,32 +41,38 @@ function listShows() {
 
     const firstDate = document.createElement('h5');
     firstDate.innerHTML = 'DATE';
+    firstDate.classList.add('shows__dates')
     showsList.appendChild(firstDate);
     
     const dateParagraph = document.createElement('p');
+    dateParagraph.classList.add('shows__paragraph')
     dateParagraph.innerHTML = details[i].when;
     showsList.appendChild(dateParagraph);
 
     const firstVenue = document.createElement('h5');
     firstVenue.innerHTML = 'VENUE';
+    firstVenue.classList.add('shows__venue')
     showsList.appendChild(firstVenue);
 
     const venueParagraph = document.createElement('p');
+    venueParagraph.classList.add('shows__para')
     venueParagraph.innerHTML = details[i].where;
     showsList.appendChild(venueParagraph);
 
     const firstLocation = document.createElement('h5');
     firstLocation.innerHTML = 'LOCATION';
+    firstLocation.classList.add('shows__location')
     showsList.appendChild(firstLocation);
 
     const locationParagraph = document.createElement('p');
+    locationParagraph.classList.add('shows__local')
     locationParagraph.innerHTML = details[i].city;
     showsList.appendChild(locationParagraph);
 
     const buyBtn = document.createElement('a');
     buyBtn.innerText = 'BUY TICKETS';
     buyBtn.setAttribute('href', '#');
-    buyBtn.classList.add('show__btn');
+    buyBtn.classList.add('shows__btn');
     buyBtn.addEventListener('click', function(event){
         event.preventDefault();
     });
