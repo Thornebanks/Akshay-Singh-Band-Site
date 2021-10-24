d3b6a1ee32c900708c16b9f1ebf19110dcdd3dde
Thornebanks <akshays.singh613@gmail.com>
2021-10-24 14:38:08 -0400

    finished all minor styling

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,83 +1,65 @@
-const details = [
-  {
-    when: "Mon Sept 06 2021",
-    where: "Ronald Lane",
-    city: "San Francisco, CA",
-  },
-  {
-    when: "Tue Sept 21 2021 ",
-    where: "Pier 3 East",
-    city: "San Francisco, CA",
-  },
-  {
-    when: "Fri Oct 15 2021",
-    where: "View Lounge",
-    city: "San Francisco, CA",
-  },
-  {
-    when: "Sat Nov 06 2021 ",
-    where: "Hyatt Agency",
-    city: "San Francisco, CA",
-  },
-  {
-    when: "Fri Nov 26 2021",
-    where: "Moscow Center",
-    city: "San Francisco, CA",
-  },
-  {
-    when: "Wed Dec 15 2021 ",
-    where: "Press Club",
-    city: "San Francisco, CA",
-  },
-];
-const shows = document.querySelector('.shows');
+const api = "https://project-1-api.herokuapp.com/";
+const key = "6771c106-cf18-40b2-a44a-fc3329e43d0e";
 
-function listShows() {
-  for (let i = 0; i < details.length; i++) {
+function getShows() {
+  axios.get(`${api}showdates?api_key=${key}`)
+  .then((response) => {
+    console.log(response);
+    listShows(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+}
+getShows();
+
+const shows = document.querySelector(".shows");
+
+function listShows(newShows) {
+  for (let i = 0; i < newShows.length; i++) {
     const showsList = document.createElement("li");
-    showsList.classList.add('shows-list__details')
-    showsList.classList.add('shows');
+    showsList.classList.add("shows-list__details");
+    showsList.classList.add("shows");
     shows.appendChild(showsList);
 
-    const firstDate = document.createElement('h5');
-    firstDate.innerHTML = 'DATE';
-    firstDate.classList.add('shows__dates')
+    const firstDate = document.createElement("h5");
+    firstDate.innerHTML = "DATE";
+    firstDate.classList.add("shows__dates");
     showsList.appendChild(firstDate);
-    
-    const dateParagraph = document.createElement('p');
-    dateParagraph.classList.add('shows__paragraph')
-    dateParagraph.innerHTML = details[i].when;
+
+    const dateParagraph = document.createElement("p");
+    dateParagraph.classList.add("shows__paragraph");
+    dateParagraph.innerHTML = new Date(parseInt(newShows[i].date)).toLocaleDateString();
     showsList.appendChild(dateParagraph);
 
-    const firstVenue = document.createElement('h5');
-    firstVenue.innerHTML = 'VENUE';
-    firstVenue.classList.add('shows__venue')
+    const firstVenue = document.createElement("h5");
+    firstVenue.innerHTML = "VENUE";
+    firstVenue.classList.add("shows__venue");
     showsList.appendChild(firstVenue);
 
-    const venueParagraph = document.createElement('p');
-    venueParagraph.classList.add('shows__para')
-    venueParagraph.innerHTML = details[i].where;
+    const venueParagraph = document.createElement("p");
+    venueParagraph.classList.add("shows__para");
+    venueParagraph.innerHTML = newShows[i].place;
     showsList.appendChild(venueParagraph);
 
-    const firstLocation = document.createElement('h5');
-    firstLocation.innerHTML = 'LOCATION';
-    firstLocation.classList.add('shows__location')
+    const firstLocation = document.createElement("h5");
+    firstLocation.innerHTML = "LOCATION";
+    firstLocation.classList.add("shows__location");
     showsList.appendChild(firstLocation);
 
-    const locationParagraph = document.createElement('p');
-    locationParagraph.classList.add('shows__local')
-    locationParagraph.innerHTML = details[i].city;
+    const locationParagraph = document.createElement("p");
+    locationParagraph.classList.add("shows__local");
+    locationParagraph.innerHTML = newShows[i].location;
     showsList.appendChild(locationParagraph);
 
-    const buyBtn = document.createElement('a');
-    buyBtn.innerText = 'BUY TICKETS';
-    buyBtn.setAttribute('href', '#');
-    buyBtn.classList.add('shows__btn');
-    buyBtn.addEventListener('click', function(event){
-        event.preventDefault();
+    const buyBtn = document.createElement("a");
+    buyBtn.innerText = "BUY TICKETS";
+    buyBtn.setAttribute("href", "#");
+    buyBtn.classList.add("shows__btn");
+    buyBtn.addEventListener("click", function (event) {
+      event.preventDefault();
     });
     showsList.appendChild(buyBtn);
-
   }
 }
-listShows();
+
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,76 +1,75 @@
-const comments = [
-    {
-        img: '',
-        name: 'Connor Walton',
-        date: '02/17/2021',
-        comment: 'This is art. This is inexplicable magic expressed in the purest     way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.'
-    },
-    {
-        img: '',
-        name: 'Emilie Beach',
-        date: '01/09/2021',
-        comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.'
-    },
-    {
-        img: '',
-        name: 'Miles Acosta',
-        date: '12/20/2020',
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+const api = "https://project-1-api.herokuapp.com/";
+const key = "6771c106-cf18-40b2-a44a-fc3329e43d0e";
 
-    },
-];
+function getComments() {
+  axios
+    .get(`${api}comments?api_key=${key}`)
+    .then((response) => {
+      console.log(response);
+      const sortedComments = response.data.sort(
+        (a, b) => b.timestamp - a.timestamp
+      );
+      displayComment(sortedComments);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+getComments();
 
+const commentsForm = document.querySelector(".comments__form");
+const commentsUnorder = document.querySelector(".comments-ul");
 
-const commentsForm = document.querySelector('.comments__form');
-const commentsUnorder = document.querySelector('.comments-ul');
+commentsForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const comment = event.target.comment.value;
+  const commenter = event.target.name.value;
 
-commentsForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const comment = event.target.comment.value;
-    const commenter = event.target.name.value;
-    function createComment(){
-        return{
-            name: commenter,
-            date: new Date(Date.now()).toLocaleDateString(),
-            comment: comment,
-        };
-    }
-    commentsUnorder.innerHTML = '';
-    commentsForm.reset();
-    comments.unshift(createComment());
-    displayComment(comments);
+  axios
+    .post(`${api}comments?api_key=${key}`, {
+      name: commenter,
+      comment: comment,
+    })
+    .then((response) => {
+      console.log(response);
+      commentsUnorder.innerHTML = "";
+      commentsForm.reset();
+      getComments();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
-function displayComment() {
-    for (let i = 0; i < comments.length; i++) {
-        const commentSection = document.createElement('li');
-        commentSection.classList.add('comments-ul__reviews');
-        commentSection.classList.add('review');
-        commentsUnorder.appendChild(commentSection);
+function displayComment(getComments) {
+  for (let i = 0; i < getComments.length; i++) {
+    const commentSection = document.createElement("li");
+    commentSection.classList.add("comments-ul__reviews");
+    commentSection.classList.add("review");
+    commentsUnorder.appendChild(commentSection);
 
-        const commentImg = document.createElement('img');
-        commentImg.classList.add('comments__img');
-        commentImg.setAttribute('src', comments[i].img);
-        commentSection.appendChild(commentImg);
-      
-        const commentName = document.createElement('h4');
-        commentName.innerText = comments[i].name;
-        commentName.classList.add('comments__reviewer');
-        commentSection.appendChild(commentName);
+    const commentImg = document.createElement("div");
+    commentImg.classList.add("comments__img");
+    commentSection.appendChild(commentImg);
 
-        const commentDate = document.createElement('h5');
-        commentDate.innerText = comments[i].date;
-        commentDate.classList.add('comments__date');
-        commentSection.appendChild(commentDate);
+    const commentName = document.createElement("h4");
+    commentName.innerText = getComments[i].name;
+    commentName.classList.add("comments__reviewer");
+    commentSection.appendChild(commentName);
 
-        const commentReview = document.createElement('p');
-        commentReview.innerText = comments[i].comment;
-        commentReview.classList.add('comments__review');
-        commentSection.appendChild(commentReview);
-        
-        commentsForm.appendChild(commentsUnorder);
-        
-    }
+    const commentDate = document.createElement("h5");
+    commentDate.innerText = new Date(
+      parseInt(getComments[i].timestamp)
+    ).toLocaleDateString();
+    commentDate.classList.add("comments__date");
+    commentSection.appendChild(commentDate);
+
+    const commentReview = document.createElement("p");
+    commentReview.innerText = getComments[i].comment;
+    commentReview.classList.add("comments__review");
+    commentSection.appendChild(commentReview);
+
+    commentsForm.appendChild(commentsUnorder);
+  }
 }
-displayComment();
 
